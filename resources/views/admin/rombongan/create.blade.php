a47e5284b4dc077c873f8a78d32c37e764cc8afc
sangsakaa <zainuri7172gmail.com>
2024-06-27 18:05:25 +0700

    form kubro

--- resources/views/admin/rombongan/create.blade.php
+++ resources/views/admin/rombongan/create.blade.php
@@ -4,7 +4,8 @@
       {{ __('Form Pendaftaran') }}
     </h2>
   </x-slot>
-  <div class="py-5">
+  <!-- kop -->
+  <div class="py-2">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="px-2 py-2">
@@ -15,81 +16,101 @@
               }
             </style>
           </div>
-          <div class="h1  px-4 py-2 text-white  flex grid-cols-1">
+          <div class="h1 px-2 py-2 text-white  flex grid-cols-1">
             <div>
-              <img src="{{ asset('img/logo.png') }}" height="100px" width="100px" alt="Example Image">
+              <img src="{{ asset('img/logo.png') }}" height="80px" width="80px" alt="Example Image">
             </div>
             <div>
               Pendaftaran Online <br>
-              Peserta Mujahadah Kubro <br>
+              <span class="  text-2xl">
+                Peserta Mujahadah Kubro <br>
+              </span>
               Muharram 1446
             </div>
           </div>
-          <div class=" grid">
-            <p class=" grid justify-text">
-              <center>
-                Selamat datang jama'ah Mujahadah Kubro, <br>
-                silahkan lakukan pengisian pendaftaran peserta dengan benar di bawah ini : <br>
-              </center>
-              NB : *berikan saran kepada panitia terhadap pelayanan dan fasilitas yang ada
-              *setelah pengisian peserta wajib mengambil kartu peserta di panitia dengan bukti formulir pendaftaran online
-              •⁠ ⁠setelah pendaftaran wajib mengambil kartu peserta di stand penerimaan tamu
-            </p>
-          </div>
-          <form action="/rombongan-kubro/kabupaten" method="post">
-            @csrf
-            <div class=" grid grid-cols-1 gap-2 py-2">
-
-              <input type="text" name="nama" placeholder=" nama ketua rombongan">
-              <select id="provinsi" name="province">
-                <option value="">Pilih Provinsi</option>
-                @foreach($provinces as $province)
-                <option value="{{ $province['code'] }}">{{ $province['name'] }}</option>
-                @endforeach
-              </select>
-              <select id="kabupaten" name="kabupaten" disabled>
-                <option value="">Pilih Kabupaten</option>
-              </select>
-              <input type="text" name="tempat_acara" placeholder=" Tempat Transit">
-              <input type="number" name="jumlah_peserta_bapak" placeholder=" jumlah_peserta_bapak">
-              <input type="number" name="jumlah_peserta_ibu" placeholder=" jumlah_peserta_ibu">
-              <input type="number" name="jumlah_peserta_remaja" placeholder=" jumlah_peserta_remaja">
-              <input type="number" name="jumlah_peserta_kanak" placeholder=" jumlah_peserta_kanak">
-              <label for="">
-                Tanggal Berangkat
-              </label>
-              <input type="date" name="tanggal_berangkat" placeholder=" tanggal_berangkat ketua rombongan">
-              <select name="kendaraan" id="">
-                <option value="">Jenis Transportasi</option>
-                <option value="Mobil">Mobil</option>
-                <option value="Bus">Bus</option>
-                <option value="Kereta Api">Kereta Api</option>
-                <option value="Pesawat">Pesawat</option>
-                <option value="Sepeda Motor">Sepeda Motor</option>
-                <option value="Elf">Mobil Elf</option>
-              </select>
-              <div>
-                <label for="">Pilih Gelombang Yang ikuti</label>
-              </div>
-              <div class=" grid-cols-1">
-                <input type="checkbox" name="gelombang_acara[]" id="gelombang1" value="Khodimul">
-                <label for="gelombang1">Gelombang 1 Khodimul</label><br>
-                <input type="checkbox" name="gelombang_acara[]" id="gelombang2" value="Ibu-Ibu">
-                <label for="gelombang2">Gelombang 2 Ibu-Ibu</label><br>
-                <input type="checkbox" name="gelombang_acara[]" id="gelombang3" value="Remaja">
-                <label for="gelombang3">Gelombang 3 Remaja</label><br>
-                <input type="checkbox" name="gelombang_acara[]" id="gelombang4" value="Kanak-Kanak">
-                <label for="gelombang4">Gelombang 4 Kanak-Kanak</label><br>
-                <input type="checkbox" name="gelombang_acara[]" id="gelombang5" value="Bapak-Bapak">
-                <label for="gelombang5">Gelombang 5 Bapak-Bapak</label><br>
-              </div>
-              <textarea name="sarat" id="" placeholder="saran"></textarea>
-            </div>
-            <button class=" bg-purple-700 text-white px-2 py-1" type="submit">Daftar Calon Peserta</button>
-          </form>
         </div>
       </div>
     </div>
+  </div>
+  <div class="">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="px-2 py-2">
+          <div class="h1 px-2 py-2 text-white  flex grid-cols-1">
+            <div class="  text-justify text-sm  ">
+              <p class=" justified-text ">
+                Selamat datang Jama'ah Mujahadah Kubro, <br>
+                Silahkan lakukan pengisian pendaftaran peserta dengan benar di bawah ini : NB : *berikan saran kepada panitia terhadap pelayanan dan fasilitas yang ada *setelah pengisian peserta wajib mengambil kartu peserta di panitia dengan bukti formulir pendaftaran online•⁠ ⁠setelah pendaftaran wajib mengambil kartu peserta di stand penerimaan tamu
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- endkop -->
+  <div class="py-5">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class=" px-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+
+        <div class=" grid">
+
+        </div>
+        <form action="/rombongan-kubro/kabupaten" method="post">
+          @csrf
+          <div class=" grid grid-cols-1 gap-2 py-2">
+
+            <input type="text" name="nama" placeholder=" nama ketua rombongan">
+            <select class="text-sm" id="provinsi" name="province">
+              <option value="">Pilih Provinsi</option>
+              @foreach($provinces as $province)
+              <option value="{{ $province['code'] }}">{{ $province['name'] }}</option>
+              @endforeach
+            </select>
+            <select class="text-sm" id="kabupaten" name="kabupaten" disabled>
+              <option value="">Pilih Kabupaten</option>
+            </select>
+            <input type="text" name="tempat_acara" placeholder=" Tempat Transit">
+            <input type="number" name="jumlah_peserta_bapak" placeholder=" jumlah_peserta_bapak">
+            <input type="number" name="jumlah_peserta_ibu" placeholder=" jumlah_peserta_ibu">
+            <input type="number" name="jumlah_peserta_remaja" placeholder=" jumlah_peserta_remaja">
+            <input type="number" name="jumlah_peserta_kanak" placeholder=" jumlah_peserta_kanak">
+            <label for="">
+              Tanggal Berangkat
+            </label>
+            <input class=" w-full" type="date" name="tanggal_berangkat" placeholder=" tanggal_berangkat ketua rombongan">
+            <select class="text-sm" name="kendaraan" id="">
+              <option value="">Jenis Transportasi</option>
+              <option value="Mobil">Mobil</option>
+              <option value="Bus">Bus</option>
+              <option value="Kereta Api">Kereta Api</option>
+              <option value="Pesawat">Pesawat</option>
+              <option value="Sepeda Motor">Sepeda Motor</option>
+              <option value="Elf">Mobil Elf</option>
+            </select>
+            <div>
+              <label for="">Pilih Gelombang Yang ikuti</label>
+            </div>
+            <div class=" grid-cols-1">
+              <input type="checkbox" name="gelombang_acara[]" id="gelombang1" value="Khodimul">
+              <label for="gelombang1">Gelombang 1 Khodimul</label><br>
+              <input type="checkbox" name="gelombang_acara[]" id="gelombang2" value="Ibu-Ibu">
+              <label for="gelombang2">Gelombang 2 Ibu-Ibu</label><br>
+              <input type="checkbox" name="gelombang_acara[]" id="gelombang3" value="Remaja">
+              <label for="gelombang3">Gelombang 3 Remaja</label><br>
+              <input type="checkbox" name="gelombang_acara[]" id="gelombang4" value="Kanak-Kanak">
+              <label for="gelombang4">Gelombang 4 Kanak-Kanak</label><br>
+              <input type="checkbox" name="gelombang_acara[]" id="gelombang5" value="Bapak-Bapak">
+              <label for="gelombang5">Gelombang 5 Bapak-Bapak</label><br>
+            </div>
+            <textarea name="sarat" id="" placeholder="saran"></textarea>
+          </div>
+          <button class=" bg-purple-700 text-white px-2 py-1" type="submit">Daftar Calon Peserta</button>
+        </form>
+      </div>
+    </div>
+  </div>
   </div>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script>
@@ -129,5 +150,4 @@
       }
     });
   </script>
-
 </x-app-layout>

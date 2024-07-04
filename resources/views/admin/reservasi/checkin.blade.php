c3f614101e797255c53087f24bad9e6514ad63a0
sangsakaa <zainuri7172gmail.com>
2024-07-05 06:06:46 +0700

    qr

--- resources/views/admin/reservasi/checkin.blade.php
+++ resources/views/admin/reservasi/checkin.blade.php
@@ -6,7 +6,36 @@
   </x-slot>
   <script src="https://unpkg.com/html5-qrcode/minified/html5-qrcode.min.js"></script>
 
-  fff
+  <div class="py-12">
+    <div class="px-4 mx-auto sm:px-4 lg:px-4">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-4">
+          <h1>QR Code Scanner</h1>
+          <div id="reader"></div>
+          <div id="result"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script>
+    function onScanSuccess(decodedText, decodedResult) {
+      // Handle the result here.
+      console.log(`Code matched = ${decodedText}`, decodedResult);
+      document.getElementById('result').innerText = `Scanned result: ${decodedText}`;
+    }
+
+    function onScanFailure(error) {
+      // Handle the scan failure, usually better to ignore and keep scanning.
+      console.warn(`Code scan error = ${error}`);
+    }
+
+    let html5QrcodeScanner = new Html5QrcodeScanner(
+      "reader", {
+        fps: 10,
+        qrbox: 250
+      });
+    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+  </script>
 
 
 </x-app-layout>

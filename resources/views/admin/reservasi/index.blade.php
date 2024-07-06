a343836996a6ff9e89ff8784ab87b22c2522bbe1
sangsakaa <zainuri7172gmail.com>
2024-07-06 07:22:31 +0700

    dlete

--- resources/views/admin/reservasi/index.blade.php
+++ resources/views/admin/reservasi/index.blade.php
@@ -6,6 +6,7 @@
   </x-slot>
 
   <div class="py-12">
+    <meta http-equiv="refresh" content="1">
     <div class="px-4 mx-auto sm:px-4 lg:px-4">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-4">
@@ -64,58 +65,7 @@
     </div>
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script>
-    $(document).ready(function() {
-      function refreshTable() {
-        $.ajax({
-          url: '/path/to/your/backend/script', // Ganti dengan URL endpoint backend Anda
-          type: 'GET',
-          dataType: 'json',
-          success: function(data) {
-            $('#data-table tbody').empty();
-            $.each(data, function(index, item) {
-              var newRow = '<tr class="border">' +
-                '<td class="border text-center px-1">' + item.id + '</td>' +
-                '<td class="border text-center px-1">' + item.kode_pendaftaran + '<br>' + item.qr_code + '</td>' +
-                '<td class="border text-center px-1">' + item.nama + '</td>' +
-                '<td class="border text-center px-1">' + item.jumlah_peserta_remaja + '</td>' +
-                '<td class="border text-center px-1">' + item.jumlah_peserta_kanak + '</td>' +
-                '<td class="border text-center px-1">' + item.jumlah_peserta_ibu + '</td>' +
-                '<td class="border text-center px-1">' + item.jumlah_peserta_bapak + '</td>' +
-                '<td class="border text-center px-1">' + item.province + ' ' + item.kabupaten + ' ' + item.kecamatan + '</td>' +
-                '<td class="border text-center px-1">' + item.gelombang_acara.join(', ') + '</td>' +
-                '<td class="border text-center px-1">' + item.tempat_acara + '</td>' +
-                '<td class="border text-center px-1">' + item.tanggal_berangkat + '</td>' +
-                '<td class="border text-center px-1">' +
-                '<form action="/reservasi-kehadiran/' + item.id + '" method="post">' +
-                '@csrf' +
-                '@method("delete")' +
-                '<button class=" ">' +
-                '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">' +
-                '<path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />' +
-                '</svg>' +
-                '</button>' +
-                '</form>' +
-                '</td>' +
-                '</tr>';
-              $('#data-table tbody').append(newRow);
-            });
-          },
-          error: function(xhr, status, error) {
-            console.error('Error: ' + status + ' - ' + error);
-          }
-        });
-      }
 
-      $('#refreshTable').on('click', function() {
-        refreshTable();
-      });
-
-      // Set interval to automatically refresh the table every 10 seconds (10000 milliseconds)
-      setInterval(refreshTable, 10000);
-    });
-  </script>
 
 
 </x-app-layout>
